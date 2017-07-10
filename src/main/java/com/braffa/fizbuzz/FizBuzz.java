7d0c63e2bbeafeebcc610ce26409ef3aa8046a29
Braffa <dave.brayfield@gmail.com>
2017-07-10 13:58:16 +0100

    Step 2

--- src/main/java/com/braffa/fizbuzz/FizBuzz.java
+++ src/main/java/com/braffa/fizbuzz/FizBuzz.java
@@ -1,22 +1,51 @@
 package com.braffa.fizbuzz;
 
 public class FizBuzz {
+
+	private static String FIZZ = "fizz";
+	private static String BUZZ = "buzz";
+	private static String SPACE = " ";
+	private static String LUCKY = "lucky"; 
 	
-	private static String FIZZ = "fizz";  
-	private static String BUZZ = "buzz"; 
-	private static String SPACE = " "; 
-	
-	public String  fizBuzz (int [] numbers) {
+	private boolean contains3 (int number) {
+		char [] chars = String.valueOf(number).toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			if (chars [i] == '3') {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private String fizBuzz(int number) {
+		if (number % 15 == 0) {
+			return FIZZ + BUZZ;
+		} else if (number % 3 == 0) {
+			return FIZZ;
+		} else if (number % 5 == 0) {
+			return BUZZ;
+		}
+		return String.valueOf(number);
+	}
+
+	public String fizBuzz(int[] numbers) {
 		StringBuffer sb = new StringBuffer();
 		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] % 15 == 0) {
-				sb.append(FIZZ + BUZZ); 
-			} else if (numbers[i] % 3 == 0 ) {
-				sb.append(FIZZ); 
-			} else if (numbers[i] % 5 == 0) {
-				sb.append(BUZZ);
+			sb.append(fizBuzz(numbers[i]));
+			if (i < numbers.length - 1) {
+				sb.append(SPACE);
+			}
+		}
+		return sb.toString();
+	}
+
+	public String  lucky (int [] numbers) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < numbers.length; i++) {
+			if (contains3 (numbers [i])) {
+				sb.append(LUCKY);
 			} else {
-				sb.append(numbers[i]); 
+				sb.append(fizBuzz (numbers[i]));
 			}
 			if (i < numbers.length - 1) {
 				sb.append(SPACE); 
--- src/test/java/com/braffa/fizbuzz/FizBuzzTest.java
+++ src/test/java/com/braffa/fizbuzz/FizBuzzTest.java
@@ -15,11 +15,20 @@ public class FizBuzzTest {
 	}
 
     @Test
-    public void testFizzBuzz () {
+    public void testFizBuzz () {
     	String expectedResult = new String  ("1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz");    	
     	int [] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
     	String result = fizBuzz.fizBuzz(numbers);
     	assertEquals(result, expectedResult);
+    }
+    
+    @Test
+    public void testLucky () {
+    	String expectedResult = new String  ("1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz");
+    	int [] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    	String result = fizBuzz.lucky (numbers);
+    	assertEquals(expectedResult, result);
+    	
     }
 
 }

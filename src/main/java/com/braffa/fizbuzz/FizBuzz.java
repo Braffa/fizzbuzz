3b78dd7a3a49e3adeddecd09248083b5719bb6ef
Braffa <dave.brayfield@gmail.com>
2017-07-10 14:16:51 +0100

    Step 3

--- src/main/java/com/braffa/fizbuzz/FizBuzz.java
+++ src/main/java/com/braffa/fizbuzz/FizBuzz.java
@@ -1,11 +1,19 @@
 package com.braffa.fizbuzz;
 
+import java.util.ArrayList;
+
 public class FizBuzz {
 
 	private static String FIZZ = "fizz";
 	private static String BUZZ = "buzz";
 	private static String SPACE = " ";
 	private static String LUCKY = "lucky"; 
+	
+	private int fizzCount = 0;
+	private int buzzCount = 0;
+	private int fizzBuzzCount = 0;
+	private int luckyCount = 0;
+	private int intCount = 0;
 	
 	private boolean contains3 (int number) {
 		char [] chars = String.valueOf(number).toCharArray();
@@ -19,12 +27,16 @@ public class FizBuzz {
 
 	private String fizBuzz(int number) {
 		if (number % 15 == 0) {
+			fizzBuzzCount++;
 			return FIZZ + BUZZ;
 		} else if (number % 3 == 0) {
+			fizzCount++;
 			return FIZZ;
 		} else if (number % 5 == 0) {
+			buzzCount++;
 			return BUZZ;
 		}
+		intCount++;
 		return String.valueOf(number);
 	}
 
@@ -43,6 +55,7 @@ public class FizBuzz {
 		StringBuffer sb = new StringBuffer();
 		for (int i = 0; i < numbers.length; i++) {
 			if (contains3 (numbers [i])) {
+				luckyCount++;
 				sb.append(LUCKY);
 			} else {
 				sb.append(fizBuzz (numbers[i]));
@@ -52,5 +65,23 @@ public class FizBuzz {
 			}
 		}
 		return sb.toString();
+	}
+	
+	public void printReport (ArrayList<String> report) {
+		for (String st : report) {
+			System.out.println(st);
+		}
+	}
+	
+	public ArrayList<String> report (int [] numbers) {
+		ArrayList<String> al = new ArrayList<String> ();
+		String st = lucky (numbers);
+		al.add(st);
+		al.add("fizz: " + fizzCount);
+		al.add("buzz: " + buzzCount);
+		al.add("fizzbuzz: " + fizzBuzzCount);
+		al.add("lucky: " + luckyCount);
+		al.add("integer: " + intCount);
+		return al;	
 	}
 }
--- src/test/java/com/braffa/fizbuzz/FizBuzzTest.java
+++ src/test/java/com/braffa/fizbuzz/FizBuzzTest.java
@@ -2,6 +2,8 @@ package com.braffa.fizbuzz;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -28,7 +30,24 @@ public class FizBuzzTest {
     	int [] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
     	String result = fizBuzz.lucky (numbers);
     	assertEquals(expectedResult, result);
-    	
     }
-
+    
+	@Test
+	public void testReport() {
+		ArrayList<String> expectedResult = new ArrayList<String> ();
+		expectedResult.add("1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz");
+		expectedResult.add("fizz: 4");
+		expectedResult.add("buzz: 3");
+		expectedResult.add("fizzbuzz: 1");
+		expectedResult.add("lucky: 2");
+		expectedResult.add("integer: 10");
+		
+		int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+				16, 17, 18, 19, 20 };
+		
+		ArrayList<String> result = fizBuzz.report(numbers);
+		assertEquals(expectedResult, result);
+		
+		fizBuzz.printReport(result);
+	}
 }
